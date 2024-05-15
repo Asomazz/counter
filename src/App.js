2178f790e731a3e14717f36a0b3e7e4ba4fe748f
A <alzemamia@gmail.com>
2024-05-15 10:43:20 +0300

    solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useRef, useEffect } from "react";
 
-function App() {
+function MoveButton() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const buttonRef = useRef(null);
+
+  const handleClick = () => {
+    const newX = position.x + getRandomArbitrary(100);
+    const newY = position.y + getRandomArbitrary(100);
+    setPosition({ x: newX, y: newY });
+  };
+
+  const getRandomArbitrary = (max) => {
+    return Math.random() * (max - -max) + -max;
+  };
+
+  useEffect(() => {
+    if (buttonRef.current) {
+      buttonRef.current.style.transform = `translate(${position.x}px, ${position.y}px)`;
+    }
+  }, [position]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <img src="https://media.tenor.com/dUFL7zBnb_oAAAAM/love-love-you.gif" />
+      <br />
+      <div
+        style={{
+          display: "flex",
+
+          width: "10%",
+          justifyContent: "space-around",
+        }}
+      >
+        <button style={{ backgroundColor: "pink" }}>yes</button>
+        <button
+          ref={buttonRef}
+          style={{
+            color: "white",
+            backgroundColor: "black",
+            transform: `translate(${position.x}px, ${position.y}px)`,
+          }}
+          onClick={handleClick}
         >
-          Learn React
-        </a>
-      </header>
+          No ;(
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default MoveButton;
