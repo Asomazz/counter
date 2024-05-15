be10ee8a6e14ad6241eea316bd04923ab9be920f
A <alzemamia@gmail.com>
2024-05-15 10:59:44 +0300

    added two images

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import React, { useState, useRef, useEffect } from "react";
 function MoveButton() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const buttonRef = useRef(null);
+  const [imageIndex, setImageIndex] = useState(0); // Track image index
 
-  const handleClick = () => {
+  const handleClick = (button) => {
     const newX = position.x + getRandomArbitrary(100);
     const newY = position.y + getRandomArbitrary(100);
     setPosition({ x: newX, y: newY });
+
+    if (button === "No") {
+      // Update image index on No click (logic from previous solution)
+      setImageIndex((prevIndex) => {
+        if (prevIndex < 2) {
+          return prevIndex + 1;
+        } else {
+          return prevIndex;
+        }
+      });
+    } else {
+      // Reset image index on Yes click
+      setImageIndex(0);
+    }
   };
 
   const getRandomArbitrary = (max) => {
@@ -21,6 +36,12 @@ function MoveButton() {
     }
   }, [position]);
 
+  const images = [
+    "https://media.tenor.com/dUFL7zBnb_oAAAAM/love-love-you.gif",
+    "https://i.pinimg.com/474x/7a/20/dd/7a20dd7e75a1f10d1d66a41e05925606.jpg",
+    "https://i.pinimg.com/474x/6e/36/17/6e3617b50bea87164c3dc22e8f5eab16.jpg",
+  ]; // Array of image URLs
+
   return (
     <div
       style={{
@@ -31,17 +52,22 @@ function MoveButton() {
         flexDirection: "column",
       }}
     >
-      <img src="https://media.tenor.com/dUFL7zBnb_oAAAAM/love-love-you.gif" />
+      <img src={images[imageIndex]} alt="Image" />{" "}
+      {/* Use image index to select image */}
       <br />
       <div
         style={{
           display: "flex",
-
           width: "10%",
           justifyContent: "space-around",
         }}
       >
-        <button style={{ backgroundColor: "pink" }}>yes</button>
+        <button
+          style={{ backgroundColor: "pink" }}
+          onClick={() => handleClick("Yes")}
+        >
+          yes
+        </button>
         <button
           ref={buttonRef}
           style={{
@@ -49,7 +75,7 @@ function MoveButton() {
             backgroundColor: "black",
             transform: `translate(${position.x}px, ${position.y}px)`,
           }}
-          onClick={handleClick}
+          onClick={() => handleClick("No")}
         >
           No ;(
         </button>
